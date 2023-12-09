660e91926e8982ada13cc61a0eacd1d6315347f5
Jakub Gubany <gubany1@stud.uniza.sk>
2023-12-09 14:28:06 +0100

    Pridanie triedy Svetlo.

---
+++ Mole/Svetlo.java
@@ -0,0 +1,37 @@
+
+/**
+ * Trieda {@code Svetlo} slúži na «doplňte opis»…
+ *
+ * @author   «meno autora»
+ * @version  «verzia alebo dátum»
+ */
+public class Svetlo
+{
+    // Nahraďte nižšie uvedený príklad vlastnými premennými…
+
+    // inštančné premenné…
+    private int x;
+
+
+    /**
+     * Konštruktory objektov triedy Svetlo.
+     */
+    public Svetlo()
+    {
+        // Inicializujte inštančné premenné.
+        x = 0;
+    }
+
+
+    /**
+     * Prázdna metóda – «tento komentár nahraďte vlastným opisom»…
+     *
+     * @param   y  opis významu parametra
+     * @return     opis významu návratovej hodnoty – napr.: súčet x a y
+     */
+    public int príkladMetódy(int y)
+    {
+        // Sem vložte príkazy tela metódy.
+        return x + y;
+    }
+}
