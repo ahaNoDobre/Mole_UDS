30612bd1995fe82d6c334e7955fd3703ee9f60a7
ChonLee1 <ostrozovic@stud.uniza.sk>
2023-12-10 18:39:01 +0100

    Vytvorenie triedy GeneratorMoli

---
+++ Mole/GeneratorMoli.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class GeneratorMoli here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GeneratorMoli {
+    /**
+     * Constructor for objects of class GeneratorMoli
+     */
+    public GeneratorMoli() {
+        // initialise instance variables
+    }
+}
