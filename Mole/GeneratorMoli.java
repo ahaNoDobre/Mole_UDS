62c710380511a2e9fdb5b049fe34c561e5b98140
ChonLee1 <ostrozovic@stud.uniza.sk>
2023-12-10 19:00:38 +0100

    Doplnanie triedy o metody a dokumentacne komentare

--- Mole/GeneratorMoli.java
+++ Mole/GeneratorMoli.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.Random;
 /**
  * Write a description of class GeneratorMoli here.
  * 
@@ -6,10 +7,29 @@
  * @version (a version number or a date)
  */
 public class GeneratorMoli {
+    private ArrayList<Mola> mole;
+    private Random generator;
     /**
-     * Constructor for objects of class GeneratorMoli
+      * Vytvorenie pomocneho array listu kde ukladame zadany pocet moli a
+      * vytvorenie instancie generator
+      * 
+      * @param pocet int velkost array listu
+      */
+    public GeneratorMoli(int pocet) {
+        this.mole = new ArrayList<Mola>(pocet);
+        this.generator = new Random();
+    }
+    /**
+     * Metoda ktora vrati zoznam instancii Mola na zaklade parametrov
+     * 
+     * @param pocet int pocet instancii moli
      */
-    public GeneratorMoli() {
-        // initialise instance variables
+    public ArrayList<Mola> zoznamMoli(int pocet) {
+        for (int i = 0; i < pocet; i++){
+            int poziciaMoleX = this.generator.nextInt(450);
+            int poziciaMoleY = this.generator.nextInt(450);
+            this.mole.add(new Mola(poziciaMoleX, poziciaMoleY));
+        }
+        return this.mole;
     }
 }
